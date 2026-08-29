8a84287645316ee783521308fcd9e1c2c3f56a93
Bakareferanmi <bakareferanmi96@gmail.com>
2026-08-29 18:26:04 +0100

    Batch 5: wire App/main with providers and nav, complete App.css

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,14 +1,22 @@
 import { Route, Routes } from 'react-router-dom'
 import { Home } from './pages/Home'
 import { VehicleDetail } from './pages/VehicleDetail'
+import { Saved } from './pages/Saved'
+import { Sell } from './pages/Sell'
+import { BottomNav } from './components/BottomNav'
 import './App.css'
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/vehicle/:id" element={<VehicleDetail />} />
-    </Routes>
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/vehicle/:id" element={<VehicleDetail />} />
+        <Route path="/saved" element={<Saved />} />
+        <Route path="/sell" element={<Sell />} />
+      </Routes>
+      <BottomNav />
+    </>
   )
 }
 
--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -4,13 +4,16 @@ import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.tsx'
 import { ToastProvider } from './hooks/useToast.tsx'
+import { VehiclesProvider } from './hooks/useVehicles.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ToastProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <VehiclesProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </VehiclesProvider>
     </ToastProvider>
   </StrictMode>,
 )
